3da2b1e11a619bb2bbc8ededd02b53b4d8c4ede4
omnisteph <omnisteph2000@gmail.com>
2023-09-18 12:25:47 +0100

    added users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VendorModule } from './modules/vendor/vendor.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [VendorModule],
+  imports: [VendorModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
